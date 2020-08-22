67c41ef9037e6338633e3865f2f30e8a83c65afd
48694 <michal.kowal1984@gmail.com>
2020-08-22 12:52:32 +0200

    hibernate bez loginow i hasla

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -1,4 +1,12 @@
 package pl.sdacademy;
 
 public class Main {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+
+
+    }
+
 }
