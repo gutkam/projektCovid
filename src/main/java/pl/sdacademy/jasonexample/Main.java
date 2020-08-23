5ed7329f4d080a877a1ecad9b1dd642af9463aa0
SzymonBorończyk <boronczyk.szymon@gmail.com>
2020-08-23 11:57:50 +0200

    // 6 godzin pracy nie poszło na marne Działające zadanie DA2

--- src/main/java/pl/sdacademy/jasonexample/Main.java
+++ src/main/java/pl/sdacademy/jasonexample/Main.java
@@ -1,32 +1,40 @@
 package pl.sdacademy.jasonexample;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        JsonParser parser = new JsonParser();
         ArrayList arrayList = new ArrayList();
+        File file = new File("src/main/resources/example.json");
+        Scanner scanner = new Scanner(file);
+        StringBuilder text = new StringBuilder();
+        while(scanner.hasNext()) {
+                text.append(scanner.nextLine());
+        }
+        //CountryCases countryCases = gson.fromJson(text.toString(), CountryCases.class);
+        JsonParser parser = new JsonParser();
 
-        Object obj = parser.parse(new FileReader("projektCovid/src/main/resources/example.json"));
+        Object obj = parser.parse(text.toString());
         JsonArray jsonArray = (JsonArray) obj;
 
-
-        for(JsonElement jsonElement : jsonArray){
-            JsonObject jsonObject = jsonElement.getAsJsonObject();
+        for(JsonElement element : jsonArray){
+            JsonObject jsonObject = element.getAsJsonObject();
 
             String country = jsonObject.get("country").getAsString();
-            int cases = jsonObject.get("cases").getAsInt();
+
+            Integer cases = jsonObject.get("cases").getAsInt();
 
             arrayList.add(new CountryCases(country,cases));
         }
 
-        arrayList.toString();
+        System.out.println(arrayList.toString());
+
+
     }
 }
