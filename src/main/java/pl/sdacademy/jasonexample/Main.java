f73d894ab07889fc169af9320a017b3f362d29ea
SzymonBorończyk <boronczyk.szymon@gmail.com>
2020-08-23 12:23:33 +0200

    dodanie zadania DA3

--- src/main/java/pl/sdacademy/jasonexample/Main.java
+++ src/main/java/pl/sdacademy/jasonexample/Main.java
@@ -10,28 +10,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-//        ArrayList arrayList = new ArrayList();
-//        File file = new File("src/main/resources/example.json");
-//        Scanner scanner = new Scanner(file);
-//        StringBuilder text = new StringBuilder();
-//        while(scanner.hasNext()) {
-//                text.append(scanner.nextLine());
-//        }
-//        JsonParser parser = new JsonParser();
-//
-//        Object obj = parser.parse(text.toString());
-//        JsonArray jsonArray = (JsonArray) obj;
-//
-//        for(JsonElement element : jsonArray){
-//            JsonObject jsonObject = element.getAsJsonObject();
-//
-//            String country = jsonObject.get("country").getAsString();
-//
-//            Integer cases = jsonObject.get("cases").getAsInt();
-//
-//            arrayList.add(new CountryCases(country,cases));
-//        }
-//
+        ArrayList arrayList = new ArrayList();
+        File file = new File("src/main/resources/example.json");
+        Scanner scanner = new Scanner(file);
+        StringBuilder text = new StringBuilder();
+        while(scanner.hasNext()) {
+                text.append(scanner.nextLine());
+        }
+        JsonParser parser = new JsonParser();
+
+        Object obj = parser.parse(text.toString());
+        JsonArray jsonArray = (JsonArray) obj;
+
+        for(JsonElement element : jsonArray){
+            JsonObject jsonObject = element.getAsJsonObject();
+
+            String country = jsonObject.get("country").getAsString();
+
+            Integer cases = jsonObject.get("cases").getAsInt();
+
+            arrayList.add(new CountryCases(country,cases));
+        }
+
         System.out.println(ApiDataProvider.createObjects("example.json").toString());
 
 
