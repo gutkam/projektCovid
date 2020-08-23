d06e08ce3779a61fe1d3814469844fe1fde972b9
SzymonBorończyk <boronczyk.szymon@gmail.com>
2020-08-23 12:21:35 +0200

    dodanie zadania DA3

--- src/main/java/pl/sdacademy/jasonexample/ApiDataProvider.java
+++ src/main/java/pl/sdacademy/jasonexample/ApiDataProvider.java
@@ -1,2 +1,68 @@
-package pl.sdacademy.jasonexample;public class ApiDataProvider {
+package pl.sdacademy.jasonexample;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ApiDataProvider {
+
+
+    public static ArrayList<CountryCases> createObjects(String fileName) throws FileNotFoundException {
+        ArrayList arrayList = new ArrayList();
+        File file = new File("src/main/resources/" + fileName);
+        Scanner scanner = new Scanner(file);
+        StringBuilder text = new StringBuilder();
+        while(scanner.hasNext()) {
+            text.append(scanner.nextLine());
+        }
+        JsonParser parser = new JsonParser();
+
+        Object obj = parser.parse(text.toString());
+        JsonArray jsonArray = (JsonArray) obj;
+
+        for(JsonElement element : jsonArray){
+            JsonObject jsonObject = element.getAsJsonObject();
+
+            String country = jsonObject.get("country").getAsString();
+
+            Integer cases = jsonObject.get("cases").getAsInt();
+
+            arrayList.add(new CountryCases(country,cases));
+        }
+        return arrayList;
+    }
+
+    public static ArrayList createObjects() throws FileNotFoundException {
+        ArrayList arrayList = new ArrayList();
+        File file = new File("src/main/resources/data.json");
+        Scanner scanner = new Scanner(file);
+        StringBuilder text = new StringBuilder();
+        while(scanner.hasNext()) {
+            text.append(scanner.nextLine());
+        }
+        JsonParser parser = new JsonParser();
+
+        Object obj = parser.parse(text.toString());
+        JsonArray jsonArray = (JsonArray) obj;
+
+        for(JsonElement element : jsonArray){
+            JsonObject jsonObject = element.getAsJsonObject();
+
+            String country = jsonObject.get("country").getAsString();
+
+            Integer cases = jsonObject.get("cases").getAsInt();
+
+            arrayList.add(new CountryCases(country,cases));
+        }
+        return arrayList;
+    }
+
+
+
 }
